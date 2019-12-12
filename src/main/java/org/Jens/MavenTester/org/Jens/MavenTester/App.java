88730c79c6b20295316b46346ba3c32fe6e68c10
Jens Roecker <j.roecker@borgel.local>
2019-12-12 14:28:57 +0100

    Init

---
+++ src/main/java/org/Jens/MavenTester/org/Jens/MavenTester/App.java
@@ -0,0 +1,13 @@
+package org.Jens.MavenTester.org.Jens.MavenTester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
